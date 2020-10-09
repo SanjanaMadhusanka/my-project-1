dc2a9fc4318b48fbf6511f188d2d9158ed338091
Oshadi Jayalath <iresha.17@business.mrt.ac.lk>
2020-10-09 22:50:25 +0530

    My commit with python file

---
+++ MYPYTHON.py
@@ -0,0 +1,10 @@
+# Python program to find largest number in a list 
+  
+# list of numbers 
+list1 = [10, 20, 4, 45, 99, 200, 199, 150, 250] 
+  
+# sorting the list 
+list1.sort() 
+  
+# printing the last element 
+print("Largest element is:", list1[-1])
