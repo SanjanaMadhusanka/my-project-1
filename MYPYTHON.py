98ad92e74b0008c2aa32421cffac37526bca8a7f
SanjanaMadhusanka <72661822+SanjanaMadhusanka@users.noreply.github.com>
2020-10-10 21:14:42 +0530

    Update MYPYTHON.py

--- MYPYTHON.py
+++ MYPYTHON.py
@@ -1,7 +1,7 @@
 # Python program to find largest number in a list 
   
 # list of numbers 
-list1 = [10, 20, 4, 45, 99, 200, 199, 150, 250] 
+list1 = [10, 20, 4, 45, 99, 200, 203, 597, ]
   
 # sorting the list 
 list1.sort() 
